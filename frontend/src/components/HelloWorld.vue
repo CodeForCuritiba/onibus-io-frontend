538da9920f67ca98a9416b93de70b020985fc730
Jonhnatha Trigueiro <joepreludian@gmail.com>
2019-11-19 00:08:10 -0300

    Add a simple makefile and implement a customized HelloWorld Vue

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -15,7 +15,7 @@
 
       <v-flex mb-4>
         <h1 class="display-2 font-weight-bold mb-3">
-          OnibusIO
+          OnibusIO - With Continuous Deployment Enabled =)
         </h1>
         <p class="subheading font-weight-regular">
           For help and collaboration with other Vuetify developers,
