cc46768ea39a6f3c34073e760559485d39ce5ba3
Luiz Augusto Volpi Nascimento <luiz.v.nasc@gmail.com>
2019-11-19 21:27:38 -0300

    Criado esboço da pagina inicial

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -4,4 +4,17 @@ import Vuetify from 'vuetify/lib'
 Vue.use(Vuetify)
 
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: '#f4511e',
+        secondary: '#424242',
+        accent: '#82B1FF',
+        error: '#FF5252',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FFC107'
+      }
+    }
+  }
 })
